a2f6b6f121f324c6555dab011da91357e65a935e
Jakub Kutnik <jakubkutnik@gmail.com>
2021-07-31 19:27:27 +0200

    Add sorting by type and seller

--- budget_master.py
+++ budget_master.py
@@ -10,6 +10,7 @@ c = conn.cursor()
 prompt = ("What would you like to do?\n1 - add expense\n"
 		"2 - remove expense\n3 - edit expense\n"
 		"4 - search for expense\n5 - list expenses\n"
+		"6 - sort expenses\n"
 )
 message = ""
 
@@ -17,22 +18,21 @@ expenses = []
 
 
 def create_database():
-	c.execute("""CREATE TABLE expenses (
+	c.execute("""CREATE TABLE if not exists expenses (
 		seller text,
 		type text,
 		price integer,
+		currency text,
 		transaction_date text
 		)""")
 
 
-def add_expense(seller, expense_type, price, transaction_date):
+def add_expense(seller, expense_type, price, transaction_date, currency="PLN"):
 	new_id = len(expenses) + 1
 	with conn:
 		c.execute("""INSERT INTO expenses VALUES
-			(:seller, :expense_type, :price, :transaction_date)""", 
-			{'seller': seller, 'expense_type': expense_type, 'price': price, 'transaction_date': transaction_date})
-	new_expense = {'id': new_id, seller: price}
-	expenses.append(new_expense)
+			(:seller, :expense_type, :price, :currency, :transaction_date)""", 
+			{'seller': seller, 'expense_type': expense_type, 'price': price, 'currency': currency, 'transaction_date': transaction_date})
 
 
 def remove_expense(rowid):
@@ -47,14 +47,18 @@ def edit_expense(rowid):
 		new_seller = input("Enter new seller: ")
 		with conn:
 			c.execute("UPDATE expenses SET seller=:seller WHERE rowid=:rowid", {'seller': new_seller, 'rowid': rowid})
-	elif answer == "expense_type":
-		new_type == input("Enter new expense type: ")
+	elif answer == "type":
+		new_type = input("Enter new expense type: ")
 		with conn:
-			c.execute("UPDATE expenses SET expense_type=:expense_type WHERE rowid=:rowid", {'expense_type': new_type, 'rowid': rowid})
+			c.execute("UPDATE expenses SET type=:type WHERE rowid=:rowid", {'type': new_type, 'rowid': rowid})
 	elif answer == "price":
 		new_price = input("Enter new price: ")
 		with conn:
 			c.execute("UPDATE expenses SET price=:price WHERE rowid=:rowid", {'price': new_price, 'rowid': rowid})
+	elif answer == "currency":
+		new_currency = input("Enter new currency: ")
+		with conn:
+			c.execute("UPDATE expenses SET currency=:currency WHERE rowid=:rowid", {'currency': new_currency, 'rowid': rowid})
 	elif answer == "transaction_date":
 		new_date = input("Enter new date: ")
 		with conn:
@@ -79,6 +83,20 @@ def list_expenses():
 		print(row, '\n')
 
 
+def sort_expenses_by_seller():
+	sorted_seller = input("Which seller would you like to sort by? ")
+	c.execute("SELECT rowid, * FROM expenses WHERE seller=:seller", {'seller': sorted_seller})
+	mytable = from_db_cursor(c)
+	print(mytable)
+
+
+def sort_expenses_by_type():
+	sorted_type = input("Which type would you like to sort by? ")
+	c.execute("SELECT rowid, * FROM expenses WHERE type=:type", {'type': sorted_type})
+	mytable = from_db_cursor(c)
+	print(mytable)
+
+
 def is_valid_date(input_date):
 	datetime.datetime.strptime(input_date, '%Y-%m-%d')
 
@@ -98,17 +116,21 @@ def is_valid_id(rowid):
 active = True
 
 while active:
-	if not file_exists('expenses.db'):
-		create_database()
+	create_database()
 	message = input(prompt)
 
 	if message == "quit":
 		active = False
 		conn.close()
 	elif str(message) == '1':
-		seller = input('Name the seller: ')
-		expense_type = input('Name the type of expense: ')
-		price = input('Name the price: ')
+		seller = input('Enter the seller: ')
+		expense_type = input('Enter the type of expense: ')
+		price = input('Enter the price: ')
+		default_currency = input('Default currency (PLN) (y/n): ')
+		if default_currency == 'y':
+			currency = None
+		elif default_currency == 'n':
+			currency = input('Enter the currency: ')
 		transaction_date = None
 		while True:
 			input_date = input('Enter the date of trasaction (yyyy-mm-dd): ')
@@ -120,7 +142,10 @@ while active:
 			else:
 				transaction_date = input_date
 				break
-		add_expense(seller, expense_type, price, transaction_date)
+		if currency:
+			add_expense(seller, expense_type, price, transaction_date, currency)
+		else:
+			add_expense(seller, expense_type, price, transaction_date)
 	elif str(message) == '2':
 		expense_id = input("Which item would you like to delete? ")
 		remove_expense(expense_id)
@@ -135,3 +160,9 @@ while active:
 		search_expenses(seller)
 	elif str(message) == '5':
 		list_expenses()
+	elif str(message) == '6':
+		sort_by = input('Would you like to sort expenses by seller or type? ')
+		if sort_by == 'seller':
+			sort_expenses_by_seller()
+		elif sort_by == 'type':
+			sort_expenses_by_type()
